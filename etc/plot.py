9d449df24069c1e9587a7f47168024f125c5c905
Victor Glazer <victor.glazer@gmail.com>
2022-12-26 17:58:30 -0500

    Produce multiple plots

--- etc/plot.py
+++ etc/plot.py
@@ -3,14 +3,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("./points_1000.csv")
+for num_points in [1000, 2000, 4000, 8000]:
+    df = pd.read_csv(f'./points_{num_points}.csv')
 
-fig, ax = plt.subplots()
+    fig, ax = plt.subplots()
 
-ax.scatter(df['x'], df['y'], alpha=0.3, s=10)
-circle = plt.Circle((0,0), 1, color='red', fill=False, label='unit circle')
-ax.add_patch(circle)
-num_points = len(df['x'])
-ax.set_title(f'{num_points:,} sample points')
+    ax.scatter(df['x'], df['y'], alpha=0.3, s=10)
+    circle = plt.Circle((0,0), 1, color='red', fill=False, label='unit circle')
+    ax.add_patch(circle)
+    ax.set_title(f'{num_points:,} sample points')
 
-fig.savefig(f'./plot_{num_points}.png')
+    fig.savefig(f'./plot_{num_points}.png')
