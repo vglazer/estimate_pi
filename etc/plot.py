faa0fa1af569b1ee855007c301dfe449df926a23
Victor Glazer <victor.glazer@gmail.com>
2022-12-28 13:06:27 -0500

    Retain point plots for diffrent num_points

--- etc/plot.py
+++ etc/plot.py
@@ -4,13 +4,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 for num_points in [1000, 2000, 4000, 8000]:
-    df = pd.read_csv(f'./points_{num_points}.csv')
+    df = pd.read_csv(f"./points_{num_points}.csv")
 
     fig, ax = plt.subplots()
 
-    ax.scatter(df['x'], df['y'], alpha=0.3, s=10)
-    circle = plt.Circle((0,0), 1, color='red', fill=False, label='unit circle')
+    ax.scatter(df["x"], df["y"], alpha=0.3, s=10)
+    circle = plt.Circle((0, 0), 1, color="red", fill=False, label="unit circle")
     ax.add_patch(circle)
-    ax.set_title(f'{num_points:,} sample points')
+    ax.set_title(f"{num_points:,} sample points")
 
-    fig.savefig(f'./plot_{num_points}.png')
+    fig.savefig(f"./plot_{num_points}.png")
